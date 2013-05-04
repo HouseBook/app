cf71ad4d5177d99e34fe9c7fa87885888ad063b7
Epic Imagine <epicimagine@icloud.com>
2013-05-04 14:03:43 -0400

    Added Collect Info (Awesome View) with Segue and button to do speech stuff.

---
+++ Housebook/Housebook/HBCollectInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  HBCollectInfoViewController.h
+//  Housebook
+//
+//  Created by Mitch Ruebush on 5/4/13.
+//  Copyright (c) 2013 HouseBookApp.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBCollectInfoViewController : UIViewController
+
+@end
---
+++ Housebook/Housebook/HBCollectInfoViewController.m
@@ -0,0 +1,40 @@
+//
+//  HBCollectInfoViewController.m
+//  Housebook
+//
+//  Created by Mitch Ruebush on 5/4/13.
+//  Copyright (c) 2013 HouseBookApp.com. All rights reserved.
+//
+
+#import "HBCollectInfoViewController.h"
+
+@interface HBCollectInfoViewController ()
+
+@end
+
+@implementation HBCollectInfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
