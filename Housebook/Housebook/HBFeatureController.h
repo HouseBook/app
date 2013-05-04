579a83f9b4d4fae5224eff7760afb8e5c062b096
kbucktothemax <kbuckley17@gmail.com>
2013-05-04 16:20:51 -0400

    jaewfjlfdal

---
+++ Housebook/Housebook/HBFeatureController.h
@@ -0,0 +1,13 @@
+//
+//  HBFeatureController.h
+//  Housebook
+//
+//  Created by Kevin Buckley on 5/4/13.
+//  Copyright (c) 2013 HouseBookApp.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBFeatureController : UIViewController
+
+@end
---
+++ Housebook/Housebook/HBFeatureController.m
@@ -0,0 +1,38 @@
+//
+//  HBFeatureController.m
+//  Housebook
+//
+//  Created by Kevin Buckley on 5/4/13.
+//  Copyright (c) 2013 HouseBookApp.com. All rights reserved.
+//
+
+#import "HBFeatureController.h"
+
+@interface HBFeatureController ()
+
+@end
+
+@implementation HBFeatureController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
