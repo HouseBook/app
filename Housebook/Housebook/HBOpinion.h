5e3ccb6a139bab872bd00805430653a223effa96
Epic Imagine <epicimagine@icloud.com>
2013-05-04 18:18:07 -0400

    JSON call working from IOS

---
+++ Housebook/Housebook/HBOpinion.h
@@ -0,0 +1,13 @@
+//
+//  HBOpinion.h
+//  Housebook
+//
+//  Created by Mitch Ruebush on 5/4/13.
+//  Copyright (c) 2013 HouseBookApp.com. All rights reserved.
+//
+
+#import "JSONModel.h"
+
+@interface HBOpinion : JSONModel
+
+@end
---
+++ Housebook/Housebook/HBOpinion.m
@@ -0,0 +1,13 @@
+//
+//  HBOpinion.m
+//  Housebook
+//
+//  Created by Mitch Ruebush on 5/4/13.
+//  Copyright (c) 2013 HouseBookApp.com. All rights reserved.
+//
+
+#import "HBOpinion.h"
+
+@implementation HBOpinion
+
+@end
