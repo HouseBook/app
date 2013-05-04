462d85e5a3ed9eb0d0b6c643e1d9170203005bcd
kbucktothemax <kbuckley17@gmail.com>
2013-05-04 10:38:57 -0400

    shell

---
+++ housebookapp/housebookapp/HBFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  HBFirstViewController.h
+//  housebookapp
+//
+//  Created by Kevin Buckley on 5/4/13.
+//  Copyright (c) 2013 HouseBookApp.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBFirstViewController : UIViewController
+
+@end
---
+++ housebookapp/housebookapp/HBSecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  HBSecondViewController.m
+//  housebookapp
+//
+//  Created by Kevin Buckley on 5/4/13.
+//  Copyright (c) 2013 HouseBookApp.com. All rights reserved.
+//
+
+#import "HBSecondViewController.h"
+
+@interface HBSecondViewController ()
+
+@end
+
+@implementation HBSecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
